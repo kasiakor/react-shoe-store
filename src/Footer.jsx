ae7b8488a55a22a9e3b062a51f1a415c9f706981
kasia.kordys <kasia.kordys@gmail.com>
2024-01-08 17:28:48 +0200

    add initial files

---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>These shoes are made for walking ...</p>
+    </footer>
+  );
+}
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <img alt="Spike Heels" src="/images/logo.jpg" />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
