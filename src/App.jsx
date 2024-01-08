586290a381d68a9de487502d45824965ea141cec
kasia.kordys <kasia.kordys@gmail.com>
2024-01-08 17:51:56 +0200

    display products

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,58 @@ import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 
+const products = [
+  {
+    id: 1,
+    category: "shoes",
+    image: "shoe1.jpg",
+    name: "Black Widow",
+    price: 94.95,
+    skus: [
+      { sku: "17", size: 7 },
+      { sku: "18", size: 8 },
+    ],
+    description: "Classic black dress high heels.",
+  },
+  {
+    id: 2,
+    category: "shoes",
+    image: "shoe2.jpg",
+    name: "Bloody Red",
+    price: 78.99,
+    skus: [
+      { sku: "28", size: 8 },
+      { sku: "29", size: 9 },
+    ],
+    description: "Party red hot chilly heels.",
+  },
+  {
+    id: 3,
+    category: "shoes",
+    image: "shoe3.jpg",
+    name: "Vintage Doll",
+    price: 145.95,
+    skus: [
+      { sku: "37", size: 7 },
+      { sku: "38", size: 8 },
+      { sku: "39", size: 9 },
+    ],
+    description: "Walk into the past.",
+  },
+];
+
 export default function App() {
-  // function renderProduct(p) {
-  //   return (
-  //     <div key={p.id} className="product">
-  //       <a href="/">
-  //         <img src={`/images/${p.image}`} alt={p.name} />
-  //         <h3>{p.name}</h3>
-  //         <p>${p.price}</p>
-  //       </a>
-  //     </div>
-  //   );
-  // }
+  function renderProduct(p) {
+    return (
+      <div key={p.id} className="product">
+        <a href="/">
+          <img src={`/images/${p.image}`} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+        </a>
+      </div>
+    );
+  }
 
   return (
     <>
@@ -30,6 +70,7 @@ export default function App() {
               <option value="9">9</option>
             </select>
           </section>
+          <section id="products">{products.map(renderProduct)}</section>
         </main>
       </div>
       <Footer />
