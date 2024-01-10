c679bc9ce6b2b1b83a5c6f7e94b0d86dcca71da0
kasia.kordys <kasia.kordys@gmail.com>
2024-01-10 18:21:51 +0200

    add client side nav

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,34 @@
+import { Link, NavLink } from "react-router-dom";
+
 export default function Header() {
   return (
     <header>
       <nav>
         <ul>
           <li>
-            <img alt="Spike Heels" src="/images/logo.jpg" />
+            <Link to="/">
+              <img alt="Spike Heels" src="/images/logo.jpg" />
+            </Link>
+          </li>
+          <li>
+            <NavLink
+              style={({ isActive }) => ({
+                color: isActive ? "purple" : "orange",
+              })}
+              to="/shoes"
+            >
+              Shoes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              style={({ isActive }) => ({
+                color: isActive ? "purple" : "orange",
+              })}
+              to="/cart"
+            >
+              Cart
+            </NavLink>
           </li>
         </ul>
       </nav>
