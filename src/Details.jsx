1a3b1f630fe32387d7db1cac2ae67997990cc322
kasia.kordys <kasia.kordys@gmail.com>
2024-01-10 19:26:43 +0200

    navigate to cart

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,15 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
 import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
-import PageNotFound from "./PageNotFound";
 
 export default function Details() {
   const { id } = useParams();
   const { data: product, loading, error } = useFetch(`products/${id}`);
+  const navigate = useNavigate();
 
   // early returns
   if (loading) return <Spinner />;
-  if(!product) return <PageNotFound/>
+  if (!product) return <PageNotFound />;
   if (error) throw error;
 
   return (
@@ -17,6 +18,11 @@ export default function Details() {
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+      <p>
+        <button className="btn btn-primary" onClick={() => navigate("/cart")}>
+          Add to cart
+        </button>
+      </p>
       <img src={`/images/${product.image}`} alt={product.category} />
     </div>
   );
