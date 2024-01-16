7f42490796ea1a4bde0460a1140bbb2fd092d01f
kasia.kordys <kasia.kordys@gmail.com>
2024-01-16 21:44:41 +0200

    store previous state with ref

--- src/services/useFetchAll.js
+++ src/services/useFetchAll.js
@@ -1,13 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function useFetchAll(urls) {
+  // compare new value to old value that was passed in
+  const prevUrls = useRef([]);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    // run only when urls change
+    if (areEqual(prevUrls.current, urls)) return;
+    prevUrls.current = urls;
     const promises = urls.map((url) =>
       fetch(baseUrl + url).then((response) => {
         if (response.ok) return response.json();
@@ -26,4 +31,11 @@ export default function useFetchAll(urls) {
   }, []);
 
   return { data, loading, error };
+}
+
+function areEqual(arr1, arr2) {
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((element, index, arr2) => element === arr2[index])
+  );
 }
