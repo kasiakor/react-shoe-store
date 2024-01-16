d95417f087b64367bf41383f4aa96cc4e72df2e0
kasia.kordys <kasia.kordys@gmail.com>
2024-01-16 20:57:40 +0200

    use useRef to remove warning setState on unmounted component

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,30 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function useFetch(url) {
+  const isMountedRef = useRef(false);
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
   // async /await
   useEffect(() => {
+    isMountedRef.current = true;
+    console.log("is component mounted?", isMountedRef.current);
     async function init() {
       try {
         const response = await fetch(baseUrl + url);
         if (response.ok) {
           const json = await response.json();
-          setData(json);
+          isMountedRef.current && setData(json);
         } else {
           throw response;
         }
       } catch (e) {
-        setError(e);
+        isMountedRef.current && setError(e);
       } finally {
-        setLoading(false);
+        isMountedRef.current && setLoading(false);
       }
     }
     init();
+    return () => {
+      isMountedRef.current = false;
+    };
   }, [url]);
 
   return { data, error, loading };
