7cca0a9091ba14aebf11e106745be135844766fc
kasia.kordys <kasia.kordys@gmail.com>
2024-01-18 18:37:41 +0200

    consume cart context

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,8 +1,11 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import Spinner from "./Spinner";
+import { CartContext } from "./cartContext";
 import useFetchAll from "./services/useFetchAll";
 
-export default function Cart({ cart, dispatch }) {
+export default function Cart() {
+  const { cart, dispatch } = useContext(CartContext);
   const navigate = useNavigate();
   const urls = cart.map((i) => `products/${i.id}`);
   const { data: products, loading, error } = useFetchAll(urls);
