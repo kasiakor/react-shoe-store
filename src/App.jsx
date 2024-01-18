af513876cee006e05599e47503a05aebbcd84bf3
kasia.kordys <kasia.kordys@gmail.com>
2024-01-18 19:48:30 +0200

    create cart provider to provide the cart context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useReducer } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Cart from "./Cart";
@@ -8,21 +7,8 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Products from "./Products";
 import { CartContext } from "./cartContext";
-import cartReducer from "./cartReducer";
-
-let initialState;
-try {
-  initialState = JSON.parse(localStorage.getItem("cart")) ?? [];
-} catch {
-  console.error("Your cart could not be parsed into JSON");
-  initialState = [];
-}
 
 export default function App() {
-  const [cart, dispatch] = useReducer(cartReducer, initialState);
-
-  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
-
   return (
     <CartContext value={{ cart, dispatch }}>
       <div className="content">
--- src/cartContext.js
+++ src/cartContext.js
@@ -1,3 +1,30 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import cartReducer from "./cartReducer";
 
 export const CartContext = React.createContext(null);
+
+// will be called only one time when the app loads
+let initialState;
+try {
+  initialState = JSON.parse(localStorage.getItem("cart")) ?? [];
+} catch {
+  console.error("Your cart could not be parsed into JSON");
+  initialState = [];
+}
+
+// this component will provide the context
+// & manage state and functions
+// & be responsible for calling useReducer
+export function CartProvider(props) {
+  const [cart, dispatch] = useReducer(cartReducer, initialState);
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
+  const contextValue = {
+    cart,
+    dispatch,
+  };
+  return (
+    <CartContext.Provider value={contextValue}>
+      {props.children}
+    </CartContext.Provider>
+  );
+}
